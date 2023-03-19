58a6da66163ae681b667a575f2aa48a81ed7ca6b
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-03-19 18:50:44 +0500

    async thunk realize request posts + loader+Error

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import { useSelector } from 'react-redux/es/exports';
 import './App.css';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
 import { userSlice } from './store/reducers/useSlice';
-
+import { useEffect } from 'react'
+import { fetchUsers } from './store/reducers/ActionCreators';
 
 function App() {
-  const { count } = useAppSelector(state => state.userReducer)
-  const { increment } = userSlice.actions;
-  const dispatch = useAppDispatch()
 
+  const dispatch = useAppDispatch()
+  const { users, isLoading, error } = useAppSelector(state => state.userReducer)
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
 
 
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(increment(10))}>increment</button>
+      {isLoading && <h1>Loading....</h1>}
+      {error && <h1>{error}</h1>}
+      {JSON.stringify(users, null, 2)}
     </div>
   );
 }
