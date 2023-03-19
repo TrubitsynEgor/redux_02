82f1c2a2f4f4ece983f39490d8dc0aaf76735877
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-03-19 17:35:38 +0500

    redux-toolkit base example + typing useDispatch/useSelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useSelector } from 'react-redux/es/exports';
 import './App.css';
-import UserList from './components/UserList';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { userSlice } from './store/reducers/useSlice';
+
 
 function App() {
+  const { count } = useAppSelector(state => state.userReducer)
+  const { increment } = userSlice.actions;
+  const dispatch = useAppDispatch()
+
+
+
   return (
     <div className="App">
-      <UserList />
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment(10))}>increment</button>
     </div>
   );
 }
--- src/store/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { userReducers } from './userReducers'
-
-export const rootReducer = combineReducers({
-	user: userReducers,
-})
-
-export type RootState = ReturnType<typeof rootReducer>
